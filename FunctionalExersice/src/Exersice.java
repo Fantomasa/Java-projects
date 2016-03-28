8a720d0935d41daf08ef2bee04766ba24b2722b3
Fantomasa <ivailoinfo@gmail.com>
2016-03-28 21:47:27 +0300

    Add folder

---
+++ FunctionalExersice/src/Exersice.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Created by Fantomasa on 28.3.2016 г..
+ */
+public class Exersice {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+
+
+    }
+}
